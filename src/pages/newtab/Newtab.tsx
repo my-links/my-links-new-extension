5cc5e803e0ec138d618df03b7481e709989d766e
Sonny <sonnyasdev@gmail.com>
2024-07-07 19:41:48 +0200

    feat: generate random iframe url param to bypass browser cache

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,20 +1,19 @@
-import React from "react";
 import "@pages/newtab/Newtab.scss";
-import withSuspense from "@src/shared/hoc/withSuspense";
 import withErrorBoundary from "@src/shared/hoc/withErrorBoundary";
+import withSuspense from "@src/shared/hoc/withSuspense";
 import useStorage from "@src/shared/hooks/useStorage";
 import remoteUrlStorage from "@src/shared/storages/remoteUrlStorage";
 
-const Newtab = () => {
+function Newtab() {
   const remoteUrl = useStorage(remoteUrlStorage);
   return (
     <iframe
-      src={remoteUrl}
+      src={`${remoteUrl}?rndstr=${Date.now()}`}
       title="MyLins app"
       style={{ height: "100%", width: "100%" }}
     />
   );
-};
+}
 
 export default withErrorBoundary(
   withSuspense(Newtab, <div> Loading ... </div>),
